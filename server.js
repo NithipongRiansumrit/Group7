8f933a8e61bb2de524c28df5da42bfd3df5cee97
smpsd-jelly <74004542+smpsd-jelly@users.noreply.github.com>
2020-11-24 02:16:48 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 var express = require('express');
 var path = require('path');
 var https = require('https');
-var http = require('http');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var logout = require('express-passport-logout');
@@ -16,7 +15,7 @@ var urlencodedParser = bodyParser.urlencoded({extended: false})
 var PORT  = process.env.PORT || 5000;
 
 var app = express();
-var obj;
+var userInfo = new Object();
 
 app.use(session({
 	secret: 'secret',
@@ -32,8 +31,39 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('login',{fname: 'Atip', lName: 'Boonyong'});
+app.get('/', function (req, res) { // หน้าlogin
+    res.render('login',{'status': true});
+
+});
+
+var udata;
+
+app.post('/login', urlencodedParser, function(req, res){
+    var data = {
+       'username': req.body.username,
+       'password': req.body.password
+    }
+    console.log(data);
+    console.log(tuApi(data));
+    async function main() {
+        try{
+            userInfo = await tuApi(data);
+            console.log(userInfo);
+            udata = userInfo;
+            if(userInfo.status == true){
+                if(userInfo.type != "student"){
+                    res.render('mainpage',udata);
+            }
+                res.render('mainStudent', udata);
+            }else {
+                console.log('wrong data');
+                res.render('login', {'status': false} );
+                }
+            }catch(error){
+                console.log(error);
+            }
+        }
+    main();
 
 });
 
@@ -41,76 +71,86 @@ app.listen(PORT, function () {
     console.log(`Listening on ${PORT}`)
 });
 
-app.get('/api', function (req, res) {
-    const queryParams = req.query;
-    console.log('param[1]:' + queryParams['user']);
-    res.send(queryParams);
-});
-
-var ssn;
-app.post('/auth', function(request ,response){
-    ssn = request.session;
-    ssn.username = request.body.username;
-    var username = request.body.username;
-    var password = request.body.password;
-
-    if(username && password){
+//tu api request
+async function tuApi(data){ 
+    return new Promise(function( resolve, reject) {
         var options = {
             'method': 'POST',
             'hostname': 'restapi.tu.ac.th',
             'path': '/api/v1/auth/Ad/verify',
             'headers': {
-                'Content-Type': 'application/json',
-                'Application-Key': 'TU516b31bfcaebea2c4d1180b23f7ff15c6abcb7971f7757786dfad18e3a7a8da058f8e9a43eddd71053fc3afa264615c8'
+            'Content-Type': 'application/json',
+            'Application-Key': 'TUd385e507f10b7b04ec0c73d853e9ae18a332e504be6a734cafe5527017c2be83b1040a9c3f22ed82a441bfa5dee16f58'
+            },
+        };
+
+        var userInfo;
+
+        var request = https.request(options, function (res) {
+            var chunks = [];
+        
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+        
+            res.on("end", function (chunk) {
+                var body = Buffer.concat(chunks);
+                userInfo = JSON.parse(body);
+                resolve(userInfo);
+            });
+        
+            res.on("error", function (error) {
+                console.error(error);
+            });
+        });
+        
+        var postData =  `{\n\t\"UserName\":\"${data.username}\",\n\t\"PassWord\":\"${data.password}\"\n}`;
+        request.write(postData);
+        
+        request.end();
+        
+    })
+}
+
+async function getStudentData(data){ 
+    return new Promise(function(resolve, reject) {
+        var options = {
+            'method': 'GET',
+            'hostname': 'restapi.tu.ac.th',
+            'path': `/api/v2/profile/std/info/?id=${data}`,
+            'headers': {
+              'Content-Type': 'application/json',
+              'Application-Key': 'TUd385e507f10b7b04ec0c73d853e9ae18a332e504be6a734cafe5527017c2be83b1040a9c3f22ed82a441bfa5dee16f58'
             }
         };
-        
+          
         var req = https.request(options, function (res) {
-        var chunks = [];
+            var chunks = [];
         
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+        
+            res.on("end", function (chunk) {
+                let body = Buffer.concat(chunks);
+                let studentData = JSON.parse(body);
+                resolve(studentData);
+            });
+        
+            res.on("error", function (error) {
+                console.error(error);
+            });
         });
         
-        res.on("end", function (chunk) {
-            var body = Buffer.concat(chunks);
-            console.log(body.toString());
-
-            obj = JSON.parse(body.toString());
-            console.log(obj.status);
-
-            if(obj.status == "FALSE"){
-                //response.send("FALSE");
-                response.redirect("/")
-                response.end();
-            }else{
-                request.body = body.toString();
-                response
-
-                if(obj.username == "6209680054"){ //staff main page
-                    response.redirect("/inboxStaff");
-                }else if(obj.username == "6209610036"){
-                    response.redirect("/mainpage");
-                }else{//student main page
-                    response.redirect("/mainpageStudent");
-                }
-                response.end();
-            }
-        });
+        req.end();
         
-        res.on("error", function (error) {
-            console.error(error);
-        });
-    });    
-    var postData =  "{\n\t\"UserName\":\"" + username + "\",\n\t\"PassWord\":\""+ password + "\"\n}";
-    req.write(postData);
-    req.end(); 
-    
-    }else{
-        response.send('pls enter username or password');
-        response.end();
-    }
-});
+    })
+}
+
+function checkJsonEmpty(obj){
+    return Object.keys(obj).length === 0 && obj.constructor === Object;
+}
+
 
 app.get('/logout', function(req, res){
     req.session.destroy();
@@ -118,28 +158,28 @@ app.get('/logout', function(req, res){
 });
 
 app.get('/inboxStaff', function(req, res) {
-    res.render('inboxForStaff');
+    res.render('inboxForStaff',udata);
     //res.render('inboxForStaff');
 });
 
 app.get('/status', function(req, res) {
-	res.render('formStatusForStu');
+	res.render('formStatusForStu',udata);
 });
 
 app.get('/mainpageStudent', function(req, res) {
-	res.render('mainStudent');
+	res.render('mainStudent',udata);
 });
 
 app.get('/stuform', function(req, res) {
-	res.render('formstd');
+	res.render('formstd',udata);
 });
 
 app.get('/mainpage', function(req, res) {
-	res.render('mainpage');
+	res.render('mainpage',udata);
 });
 
-app.get('/formhistory', function(req, res) {
-	res.render('formhistory');
+app.get('/profilestd', function(req, res) {
+	res.render('profilestd',udata);
 });
 
 var detailform= new Object();
@@ -168,132 +208,20 @@ app.post('/formstd2', urlencodedParser , function(req,res){
     }
     );
 
-
-app.post('/formhistory', urlencodedParser , function(req,res){
-    detailform = {
-    'to' : req.body.to,
-    'nameto': req.body.nameto,
-    'title': req.body.title,
-    'namelastname': req.body.namelastname,
-    'idstd': req.body.idstd,
-    'classstd': req.body.classstd,
-    'department': req.body.department,
-    'address': req.body.address,
-    'tumbon': req.body.tumbon,
-    'aumpor': req.body.aumpor,
-    'city': req.body.city,
-    'phonenumber': req.body.phonenumber,
-    'semester1': req.body.semester1,
-    'semester2': req.body.semester2,
-    'subjectnum': req.body.subjectnum,
-    'subject': req.body.subject,
-    'section': req.body.section,
-    'reason': req.body.reason,
-    'file': req.body.file
+var profilestd;
+app.post('/profilestd2', urlencodedParser , function(req,res){
+    profilestd ={
+        'phonenum' : req.body.phonenum,
+        'address' : req.body.address
     }
-    res.render('formhistory',detailform);
-    });
+    res.render('profilestd2',profilestd);
+
+});
 
     
-    app.get('/detailforms' , urlencodedParser ,function(req,res){    
+app.get('/detailforms' , urlencodedParser ,function(req,res){    
         res.render('detailforms1');
     });
-    app.get('/detailforms2' , urlencodedParser ,function(req,res){    
+app.get('/detailforms2' , urlencodedParser ,function(req,res){    
         res.render('detailforms2');
     });
-
-/*var options = {
-    'method': 'POST',
-    'hostname': 'restapi.tu.ac.th',
-    'path': '/api/v1/auth/Ad/verify',
-    'headers': {
-        'Content-Type': 'application/json',
-        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-    }
-}
-var req = https.request(options, function (res) {
-    var chunks = []
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
-    })
-    res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-    })
-    res.on("error", function (error) {
-        console.error(error);
-    });
-})*/
-
-
-//var options = {
-//    'method': 'GET',
-//    'hostname': 'restapi.tu.ac.th',
-//    'path': '/api/v2/std/fac/all',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//        'Application-Key': 'TUa4e553b83aa271d3411a4ad88395265801fcfb074110e8b0e03962c01f2aed6ab1662db3a0e1451df7835880c6828fcf'
-//    }
-//}
-//var req = https.request(options, function (res) {
-//    var chunks = []
-//    res.on("data", function (chunk) {
-//        chunks.push(chunk);
-//    })
-//    res.on("end", function (chunk) {
-//        var body = Buffer.concat(chunks);
-//        console.log(body.toString());
-//    })
-//    res.on("error", function (error) {
-//        console.error(error);
-//    });
-//})
-
-/*var postData =  "{\n\t\"UserName\":\"6209680055\",\n\t\"PassWord\":\"4409817425298\"\n}";
-var a = req.write(postData);
-req.end()*/
-
-//const options = {
-//    hostname: 'jsonplaceholder.typicode.com',
-//    path: '/posts/1/comments',
-//    method: 'GET',
-//    'headers': {
-//        'Content-Type': 'application/json',
-//    }
-//};
-//
-//function dataCounter(inputs) {
-//    let counter = 0;
-//    for (const input of inputs) {
-//        if (input.postId === 1) {
-//            counter += 1;
-//            console.log('input.postId:' + input.postId);
-//            console.log('input.email:' + input.email);
-//        }
-//    }
-//    return counter;
-//};
-//
-//const req = http.request(options, function(response) {
-//    response.setEncoding('utf8');
-//    var body = '';
-//    response.on('data', chunk => {
-//        body += chunk;
-//    });
-//
-//    response.on('end', () => {
-//        console.log('body:' + body);
-//        var data = JSON.parse(body);
-//        console.log('number of posts:' + dataCounter(data));
-//        console.log('data:' + data);
-//        console.log('data[0]:' + data[0]);
-//        console.log('data[0].id:' + data[0].id);
-//        console.log('data[0].email:' + data[0].email);
-//        console.log('end of GET request');
-//    });
-//});
-//
-//req.on('error', e => {
-//    console.log('Problem with request:', e.message);
-//});
-//req.end();
